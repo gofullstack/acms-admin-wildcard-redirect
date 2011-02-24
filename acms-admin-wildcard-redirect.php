42282cc774f54b3f6bcb825ffd54e9fb45e0335e
Nathan L Smith <nlloyds@gmail.com>
2011-02-24 16:29:45 -0600

    updgrade for wp 3.1

--- acms-admin-wildcard-redirect.php
+++ acms-admin-wildcard-redirect.php
@@ -2,12 +2,12 @@
 /*
 Plugin Name: Average CMS Wildcard Admin Redirect
 Plugin URI: https://github.com/cramerdev/acms-wildcard-admin-redirect
-Description: Redirects the admin pages for a mapped domain to the non-mapped version of the domain. This is required because when FORCE_SSL_ADMIN is enabled, by default the redirect will go the the https version of the URL you're on. If you're using domain mapping, that domain might not have a valid SSL certificate. This plugin redirects to the original, non-mapped domain instead.
-Version: 0.0.1
+Description: Redirects the admin pages for a mapped domain to the non-mapped version of the domain. This is required because when FORCE_SSL_ADMIN is enabled, by default the redirect will go the the https version of the URL you're on. If you're using domain mapping, that domain might not have a valid SSL certificate. This plugin redirects to the original, non-mapped domain instead. This plugin MUST be loaded AFTER the wordpress-mu-domain-mapping plugin. One way to do this is to rename this file prefixed with a z_ or something.
+Version: 0.0.2
 Author: Nathan L Smith
 */
 
-if (force_ssl_admin() && function_exists('get_original_url')) {
+if (FORCE_SSL_ADMIN && function_exists('get_original_url')) {
     function acms_wildcard_admin_redirect($location, $status = 302) {
         $url = parse_url($location);
         if ($url['scheme'] === 'https' &&
